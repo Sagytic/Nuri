e7930296ce1bd7e72dbd503eb4ffcb319a56834b
arctainable <arctain369@gmail.com>
2022-04-22 11:11:56 +0900

    style:[BE]폴더이름 변경

---
+++ backend/src/main/java/com/nuri/api/request/UserUpdatePostReq.java
@@ -0,0 +1,28 @@
+
+package com.nuri.api.request;
+
+        import io.swagger.annotations.ApiModel;
+        import io.swagger.annotations.ApiModelProperty;
+        import lombok.Getter;
+        import lombok.Setter;
+
+/**
+ * 회원 정보 수정 API ([PATCH] /api/v1/users) 요청에 필요한 리퀘스트 바디 정의.
+ */
+@Getter
+@Setter
+@ApiModel("UserUpdatePostRequest")
+public class UserUpdatePostReq {
+    String userPhone;
+    Integer isAdmin;
+    String userNickname;
+
+    @Override
+    public String toString() {
+        return "UserUpdatePostReq{" +
+                "userPhone='" + userPhone + '\'' +
+                ", isAdmin='" + isAdmin + '\'' +
+                ", userNickname='" + userNickname + '\'' +
+                '}';
+    }
+}
