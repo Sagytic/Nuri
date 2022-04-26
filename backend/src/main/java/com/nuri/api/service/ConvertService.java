dae34d40d01ae2574a6747fd97d8f308e5c6a868
홍종현 <jonghyun_h@kakao.com>
2022-04-26 18:37:52 +0900

    feat : [front][back] 자바 코드 실행

--- backend/src/main/java/com/nuri/api/service/ConvertService.java
+++ backend/src/main/java/com/nuri/api/service/ConvertService.java
@@ -7,8 +7,12 @@ import java.util.ArrayList;
 
 @Service
 public class ConvertService {
-    public ArrayList<String> convert(Long id, String userCode, Long mathGameId) {
+    public String convert(Long id, String userCode, Long mathGameId) {
         ArrayList tokens = Convert.lexical(userCode);
-        return tokens;
+        String tokenCode = "";
+        for(int i=0; i<tokens.size(); i++){
+            tokenCode += tokens.get(i);
+        }
+        return tokenCode;
     }
 }
