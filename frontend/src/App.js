63dae4ff8ab8a1d204e8de5ed466b54bedbc00c1
jinyong <zhaqh146@naver.com>
2022-04-26 11:01:57 +0900

    feat:[front] 네비게이션 바

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import Landing from './pages/landing/Landing';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Outlet />
-        <Landing />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Outlet />
     </div>
   );
 }
