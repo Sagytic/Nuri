0f5a1fb684c820a37fbc8869eb41cf68108e4a3b
jinyong <zhaqh146@naver.com>
2022-04-26 11:01:57 +0900

    feat:[front] 네비게이션 바

--- frontend/src/components/main/Nav.js
+++ frontend/src/components/main/Nav.js
@@ -1,29 +1,55 @@
-import { useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { FiMenu } from 'react-icons/fi';
 import "./Nav.css"
 
 function Nav() {
   const navigate = useNavigate();
-  let windowWidth = useRef();
-  windowWidth.current = window.innerWidth;
-  console.log(windowWidth.current);
+  const [menuIconShow, setMenuIconShow] = useState(window.innerWidth <= 760 ? true : false);
+  
   function moveLogin() {
     navigate("/user/login")
   }
 
+  function resizeHandler() {
+    if (window.innerWidth <= 760) {
+      setMenuIconShow(true)
+    } else {
+      setMenuIconShow(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeHandler);
+    return function() {
+      window.removeEventListener('resize', resizeHandler);
+    }
+  }, [])
+
   return (
     <div className="Nav">
-      <div>{ windowWidth.current }</div>
-      <FiMenu />
       <NavLink className="Nav-logo" to="/" >누리</NavLink>
-      <div className="Nav-items">
-        <NavLink className="Nav-item" to="/intro" >누리소개</NavLink>
-        <NavLink className="Nav-item" to="/reference" >살펴보기</NavLink>
-        <NavLink className="Nav-item" to="/game" >게임하기</NavLink>
-        <NavLink className="Nav-item" to="/math" >문제풀기</NavLink>
+      <div className="Nav-content">
+        {menuIconShow
+        ? 
+        <>
+          <FiMenu size="2rem"/>
+          <div className="Nav-items">
+            <NavLink className="Nav-item" to="/intro" >누리소개</NavLink>
+            <NavLink className="Nav-item" to="/reference" >살펴보기</NavLink>
+            <NavLink className="Nav-item" to="/game" >게임하기</NavLink>
+            <NavLink className="Nav-item" to="/math" >문제풀기</NavLink>
+          </div>
+        </>
+        : 
+        <div className="Nav-items">
+          <NavLink className="Nav-item" to="/intro" >누리소개</NavLink>
+          <NavLink className="Nav-item" to="/reference" >살펴보기</NavLink>
+          <NavLink className="Nav-item" to="/game" >게임하기</NavLink>
+          <NavLink className="Nav-item" to="/math" >문제풀기</NavLink>
+        </div>}
+        <button className="Nav-item-button" onClick={() => moveLogin()}>로그인</button>
       </div>
-      <button className="Nav-item-button" onClick={() => moveLogin()}>로그인</button>
     </div>
   )
 }
