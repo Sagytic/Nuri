8cb34b4b9624597d2d1f23fd1ecf17185db41212
jibin Jang <jb9992@daum.net>
2022-05-06 07:08:00 +0900

    hotfix: [front] landing/top 버그 수정

--- frontend/src/components/main/ToTopBtn.js
+++ frontend/src/components/main/ToTopBtn.js
@@ -1,19 +1,54 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import "./ToTopBtn.css"
 
-function toTopBtn() {
+// function toTopBtn() {
 
-  function toTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth'})
-  }
+//   function toTop() {
+//     window.scrollTo({ top: 0, behavior: 'smooth'})
+//   }
+
+//   return (
+//   <div className="ToTop-Container">
+//     <button onClick={toTop} className="Top-Btn">
+//       <img src="./logo192.png" alt="character"/>top으로
+//     </button>
+//   </div>
+//   )
+// }
+
+// export default toTopBtn;
+// import "./App.css";
+
+const ToTop = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // for smoothly scrolling
+    });
+  };
 
   return (
-  <div className="ToTop-Container">
-    <button onClick={toTop} className="Top-Btn">
-      <img src="./logo192.png" alt="character"/>top으로
-    </button>
-  </div>
-  )
-}
+    <>
+      {showButton && (
+        <button onClick={scrollToTop} className="back-to-top">
+          임시TOP
+        </button>
+      )}
+    </>
+  );
+};
 
-export default toTopBtn;
+export default ToTop;
