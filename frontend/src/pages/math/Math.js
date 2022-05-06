7149e7dbcaac1206ff090975653d9a77fb9beef1
jibin Jang <jb9992@daum.net>
2022-05-06 17:30:17 +0900

    feature: [front] mathgame page 구성

--- frontend/src/pages/math/Math.js
+++ frontend/src/pages/math/Math.js
@@ -1,7 +1,48 @@
 import React from "react";
+import MathList from '../../components/math/MathList'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import './Math.css'
 
 function Math() {
-  return <div>math page</div>
+  const [Alignments, setAlignments] = React.useState('');
+
+  const handleChange = (event) => {
+    setAlignments(event.target.value);
+    console.log(event.target.value);
+  };
+  return (
+    <div>
+      <div className="contents-title">
+        <h2>문제 풀러가기 </h2>
+        <hr />
+      </div>
+
+      <>
+      <Box sx={{ minWidth: 20 }}>
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">정렬</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={Alignments}
+            defaultValue={Alignments}
+            label="Alignments"
+            onChange={handleChange}
+          >
+            <MenuItem value={'newest'}>최신순</MenuItem>
+            <MenuItem value={'popular'}>조회순</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      </>
+
+      <MathList />
+    </div>
+  )  
 }
 
 export default Math
