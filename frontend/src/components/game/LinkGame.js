95b40c87e179893fef79176ce4e4128108d8d99e
jibin Jang <jb9992@daum.net>
2022-05-19 16:16:38 +0900

    refactor: [front] link game 4x4 설정

--- frontend/src/components/game/LinkGame.js
+++ frontend/src/components/game/LinkGame.js
@@ -8,10 +8,22 @@ function LinkGame({ start, finishGame }) {
     {id: 1, pair: 0, title: "int"},
     {id: 2, pair: 1, title: "소수"},
     {id: 3, pair: 1, title: "double"},
+    {id: 4, pair: 2, title: "최대"},
+    {id: 5, pair: 2, title: "max"},
+    {id: 6, pair: 3, title: "최소"},
+    {id: 7, pair: 3, title: "min"},
+    {id: 8, pair: 4, title: "구간반복"},
+    {id: 9, pair: 4, title: "for"},
+    {id: 10, pair: 5, title: "조건반복"},
+    {id: 11, pair: 5, title: "while"},
+    {id: 12, pair: 6, title: "만약 / 아니면"},
+    {id: 13, pair: 6, title: "if / else"},
+    {id: 14, pair: 7, title: "무작위"},
+    {id: 15, pair: 7, title: "random"},
   ]
 
   
-  const [selected, setSelected] = useState(new Array(4).fill(false));
+  const [selected, setSelected] = useState(new Array(16).fill(false));
   const [gameArray, setGameArray] = useState(pairArray);
   const count = useRef(0);
   const beforeId = useRef(-1);
@@ -66,7 +78,7 @@ function LinkGame({ start, finishGame }) {
 
   function newGame() {
     setGameArray(pairArray.sort(() => 0.5 - Math.random()));
-    setSelected(new Array(4).fill(false));
+    setSelected(new Array(16).fill(false));
     count.current = 0
     beforePair.current = -1
     beforeId.current = -1
